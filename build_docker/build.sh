a0e1a0dc4a1b27622df15661c7a5392947499388
AndrewF <andrew@sytrue.com>
2022-11-11 20:31:08 -0500

    simplify

--- build_docker/build.sh
+++ build_docker/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir app
-git clone ${REPO} app
 echo "${DOCKERHUB_PASS}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 docker build -t andrewduke51/nodeapp:latest .
 docker push andrewduke51/nodeapp:latest
