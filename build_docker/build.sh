dc27d18513cc23f61ee9921c40661c9615a2a1d1
AndrewF <andrew@sytrue.com>
2022-11-11 20:18:34 -0500

    Ensure user docker exists

--- build_docker/build.sh
+++ build_docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir app
+git clone ${REPO} app
 echo "${DOCKERHUB_PASS}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 docker build -t andrewduke51/nodeapp:latest .
 docker push andrewduke51/nodeapp:latest
